d784fa9a524693c353b0b41ccf432f4f4f597db8
Clayten Hamacher <chamacher@salesforce.com>
2018-05-06 10:38:53 -0700

    works - fixed setters, improved unit handling

--- blackhole.rb
+++ blackhole.rb
@@ -14,12 +14,15 @@ end
 
 class Blackhole
   # https://arxiv.org/abs/0908.1803 - Blackhole Starships
+  # https://en.wikipedia.org/wiki/Black_hole_thermodynamics
   # https://en.wikipedia.org/wiki/Hawking_radiation
   # https://en.wikipedia.org/wiki/Gravitational_constant
   # http://xaonon.dyndns.org/hawking/hrcalc.js
+  # https://www.vttoth.com/CMS/physics-notes/311-hawking-radiation-calculator
   # https://www.vttoth.com/HAWKING/hrcalc.js
   # solar mass = 2e30kg
 
+  def self.R a,b; Rational a, b end
   def self.U s  ; Unit.new s end
   def self.G    ; U("#{Physics::FundamentalConstants.newtonian_constant_of_gravitation} m^3/kg*s^2") end # m^3*kg^-1*s^-2
   def self.C    ; U("#{Physics::FundamentalConstants.speed_of_light_in_vacuum} m/s") end
@@ -37,7 +40,8 @@ class Blackhole
   def 𝜏 ; tau end
   # FIXME add superscripts ² 
 
-  attr_reader :mass_scale
+  def base_mass_scale ; 'kg' end
+  def mass_scale ; @mass_scale ||= base_mass_scale end
   def set_mass_scale u
     raise ArgumentError, "Improper argument - not mass. #{u}" unless U(u).kind == :mass
     @mass_scale = u
@@ -45,18 +49,18 @@ class Blackhole
   end
   def mass= mas
     mas = U(mas) if mas.is_a? String
-    if mas.is_a? Unit
+    if mas.is_a?(Unit) && mas.kind != :unitless
       set_mass_scale mas.units # Only set the units if they were provided
     else
       mas = U("#{mas} #{mass_scale}") # Raw numbers are treated as units of the default scale
     end
-    p [:m=, mas]
-    @mass = mas.convert_to(mas.base)
+    @mass = mas
   end
   alias setmass mass=
   def mass ; @mass end
 
-  attr_reader :radius_scale
+  def base_radius_scale ; 'm' end
+  def radius_scale ; @radius_scale ||= base_radius_scale end
   def set_radius_scale u
     raise ArgumentError, "Improper argument - not length. #{u}" unless U(u).kind == :length
     @radius_scale = u
@@ -64,24 +68,25 @@ class Blackhole
   def radius= rad
     rad = U(rad) if rad.is_a? String
     set_radius_scale rad.units
-    setmass(rad / (2 * g) * c**2)
+    setmass(rad.convert_to(base_radius_scale) / (2 * g) * c**2)
   end
   def radius ; (mass * 2 * g / c**2).convert_to(radius_scale) end
 
-  attr_reader :area_scale
+  def base_area_scale ; 'm^2' end
+  def area_scale ; @area_scale ||= base_area_scale end
   def set_area_scale u
     raise ArgumentError, "Improper argument - not area. #{u}" unless U(u).kind == :area
     @area_scale = u
   end
   def area= ar
     ar = U(ar) if ar.is_a? String
-    mas = Math.sqrt(ar / (16 * pi * g**2) * (c**4) )
     set_area_scale ar.units
-    setmass mas
+    setmass(Math.sqrt(ar.convert_to(base_area_scale) / (16 * pi * g**2) * (c**4)))
   end
   def area ; (4 * pi * radius**2).convert_to(area_scale) end
 
-  attr_reader :gravity_scale
+  def base_gravity_scale ; 'm/s^2' end
+  def gravity_scale ; @gravity_scale ||= base_gravity_scale end
   def set_gravity_scale u
     raise ArgumentError, "Improper argument - not acceleration. #{u}" unless U(u).kind == :acceleration
     @gravity_scale = u
@@ -89,39 +94,12 @@ class Blackhole
   def gravity= grav
     grav = U(grav) if grav.is_a? String
     set_gravity_scale grav.units
-    setmass(1 / (grav / c**4 * (4 * g)))
+    setmass(1 / (grav.convert_to(base_gravity_scale) / c**4 * (4 * g)))
   end
   def gravity ; (1 / mass * c**4 / (4 * g)).convert_to(gravity_scale) end
 
-  attr_reader :luminosity_scale
-  def set_luminosity_scale u
-    raise ArgumentError, "Improper argument - not power. #{u}" unless U(u).kind == :power
-    @luminosity_scale = u
-  end
-  def luminosity= lum
-    lum = U(lum) if lum.is_a? String
-    set_luminosity_scale lum.units
-    mas1 = lum / (hbar * c**6) * (15360 * pi * g**2)
-    mas2 = 1 / Math.sqrt(mas1)
-    setmass mas2
-  end
-  def luminosity ; ((1 / mass**2) * ((hbar * c**6) / (15360 * pi * g**2))) end
-
-  attr_reader :lifetime_scale
-  def set_lifetime_scale u
-    raise ArgumentError, "Improper argument - not time. #{u}" unless U(u).kind == :time
-    @lifetime_scale = u
-  end
-  def lifetime= lif
-    lif = U(lif) if lif.is_a? String
-    set_lifetime_scale lif.units
-    mas1 = lif / (5120 * pi * g**2) * (hbar * c**4)
-    mas2 = mas1**Rational(1,3)
-    setmass mas2
-  end
-  def lifetime ; (mass**3 * ((5120 * pi * g**2) / (hbar * c**4))).convert_to(lifetime_scale) end
-
-  attr_reader :energy_scale
+  def base_energy_scale ; 'J' end
+  def energy_scale ; @energy_scale ||= base_energy_scale end
   def set_energy_scale u
     raise ArgumentError, "Improper argument - not energy. #{u}" unless U(u).kind == :energy
     @energy_scale = u
@@ -129,9 +107,35 @@ class Blackhole
   def energy= energ
     energ = U(energ) if energ.is_a? String
     set_energy_scale energ.units
-    setmass(energ.convert_to('J') / c**2)
+    setmass(energ.convert_to(base_energy_scale) / c**2)
   end
   def energy ; (mass * c**2).convert_to(energy_scale) end
+
+  def base_luminosity_scale ; 'W' end
+  def luminosity_scale ; @luminosity_scale ||= base_luminosity_scale end
+  def set_luminosity_scale u
+    raise ArgumentError, "Improper argument - not power. #{u}" unless U(u).kind == :power
+    @luminosity_scale = u
+  end
+  def luminosity= lum
+    lum = U(lum) if lum.is_a? String
+    set_luminosity_scale lum.units
+    setmass(1 / Math.sqrt(lum.convert_to(base_luminosity_scale) / (hbar * c**6) * (15360 * pi * g**2)))
+  end
+  def luminosity ; ((1 / mass**2) * ((hbar * c**6) / (15360 * pi * g**2))).convert_to(luminosity_scale) end
+
+  def base_lifetime_scale ; 's' end
+  def lifetime_scale ; @lifetime_scale ||= base_lifetime_scale end
+  def set_lifetime_scale u
+    raise ArgumentError, "Improper argument - not time. #{u}" unless U(u).kind == :time
+    @lifetime_scale = u
+  end
+  def lifetime= lif
+    lif = U(lif) if lif.is_a? String
+    set_lifetime_scale lif.units
+    setmass((lif.convert_to(base_lifetime_scale) / 5120 / pi / g**2 * hbar * c**4).base ** R(1,3))
+  end
+  def lifetime ; (mass**3 * ((5120 * pi * g**2) / (hbar * c**4))).convert_to(lifetime_scale) end
 
   def entropy= n ; raise end # FIXME
   def entropy_unitcorrection ; '1 kg*m^2/W*s^3' end
@@ -147,13 +151,6 @@ class Blackhole
 
   def initialize mass # kg
     self.mass= mass
-    @mass_scale = 'kg'
-    @radius_scale = 'm'
-    @area_scale = 'm^2'
-    @gravity_scale = 'm/s^2'
-    @luminosity_scale = 'W'
-    @lifetime_scale = 's'
-    @energy_scale = 'J'
   end
 end
 
